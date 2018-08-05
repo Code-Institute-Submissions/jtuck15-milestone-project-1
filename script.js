d1558d8e93f11f4bbdb9fb373c84db460b223953
Jimmy Tuck <jim.tuck15@gmail.com>
2018-08-05 23:55:53 +0000

    finished music and booking section

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// Read More Button in Home Page
+
+$(document).ready(function() {
+  $("#toggle").click(function() {
+    var elem = $("#toggle").text();
+    if (elem == "Read More") {
+      //Stuff to do when btn is in the read more state
+      $("#toggle").text("Read Less");
+      $("#text").slideDown();
+    } else {
+      //Stuff to do when btn is in the read less state
+      $("#toggle").text("Read More");
+      $("#text").slideUp();
+    }
+  });
+});
+
+// End of Read More Button in Home Page
