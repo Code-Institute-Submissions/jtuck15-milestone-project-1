d68f7d3630d6f2f258f0003c549327431737131c
Jimmy Tuck <jim.tuck15@gmail.com>
2018-08-08 11:08:48 +0000

    all sections nearly finished just mainly styling left to do

--- script.js
+++ script.js
@@ -16,4 +16,22 @@ $(document).ready(function() {
   });
 });
 
-// End of Read More Button in Home Page
+
+// Home Button Fade in and Out
+
+    $(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#homeBtn').fadeIn(); 
+        } else { 
+            $('#homeBtn').fadeOut(); 
+        } 
+    }); 
+    
+// Home Button Smooth Scroll
+
+    $('#homeBtn').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
